fdf55945715ae6df9cbbee5b7f7fcbc59482f97a
BVeronezi <bds.veronezi@gmail.com>
2018-09-20 23:30:05 -0300

    Commit viewmodel

--- Drugstore.Web/ViewModels/CadastroUsuarioViewModel.cs
+++ Drugstore.Web/ViewModels/CadastroUsuarioViewModel.cs
@@ -4,27 +4,27 @@ namespace Drugstore.Web.ViewModels
 {
     public class CadastroUsuarioViewModel
     {
-        [Required(ErrorMessage = "Informe seu e-mail")]
+        [Required(ErrorMessage = "Campo obrigatório")]
         [Display(Name = "E-mail")]
         public string Email { get; set; }
 
-        [Required(ErrorMessage = "Informe seu nome")]
+        [Required(ErrorMessage = "Campo obrigatório")]
         [MaxLength(100, ErrorMessage ="O nome deve ter até 100 caracteres")]
         [Display(Name = "Nome")]
         public string Nome { get; set; }
 
-        [Required(ErrorMessage = "Informe seu Sobrenome")]
+        [Required(ErrorMessage = "Campo obrigatório")]
         [Display(Name = "Sobrenome")]
         public string Sobrenome { get; set; }
 
-        [Required(ErrorMessage = "Informe seu Login")]
+        [Required(ErrorMessage = "Campo obrigatório")]
         [MaxLength(50, ErrorMessage = "O nome deve ter até 50 caracteres")]
         public string Login { get; set; }
 
-        [Required(ErrorMessage = "informe seu cpf")]
-        public int CPF { get; set; }
+        [Required(ErrorMessage = "Campo obrigatório")]
+        public string CPF { get; set; }
         
-        [Required(ErrorMessage = "Informe sua senha")]
+        [Required(ErrorMessage = "Campo obrigatório")]
         [DataType(DataType.Password)]
         public string Senha { get; set; }
 
